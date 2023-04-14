f170ee1dbd7a0a5265f3e937c854c069ec49518f
최은영 <76518850+silver0-stack@users.noreply.github.com>
2023-04-14 21:19:00 +0900

    UserManagement 삭제

--- src/main/java/com/example/ama_backend/entity/AnswerEntity.java
+++ src/main/java/com/example/ama_backend/entity/AnswerEntity.java
@@ -13,6 +13,7 @@ public class AnswerEntity {
     private String nickName;  // 유저의 닉네임
     private String answerText; // 답변 내용
     private LocalDateTime createdTime; //답변이 올라온 시간
+
     // @ManyToOne 어노테이션을 사용하여 QuestionEntity 클래스의 answers 프로퍼티와 매핑
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "question_id", referencedColumnName = "id")
