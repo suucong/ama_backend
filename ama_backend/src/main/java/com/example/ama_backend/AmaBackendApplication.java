af036cbc205649bd72a2a4b56eccb60435b76aff
최은영 <76518850+silver0-stack@users.noreply.github.com>
2023-04-14 20:32:57 +0900

    QuestionEntity, AnswerEntity 생성

---
+++ ama_backend/src/main/java/com/example/ama_backend/AmaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.ama_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmaBackendApplication.class, args);
+	}
+
+}
