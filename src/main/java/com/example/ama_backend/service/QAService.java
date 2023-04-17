0dff6258c41ad7abb80fdc64aaf846c01e76f72e
최은영 <76518850+silver0-stack@users.noreply.github.com>
2023-04-17 21:08:11 +0900

    add : QAService

--- src/main/java/com/example/ama_backend/service/QAService.java
+++ src/main/java/com/example/ama_backend/service/QAService.java
@@ -11,6 +11,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @Service
@@ -39,19 +40,26 @@ public class QAService {
     }
 
     // 내가 한 질문과 답변을 조회하는 기능
-    public List<QuestionEntity> getMyQuestions(String userId) {
+    public List<QuestionEntity> getMyQuestions(final String userId) {
         return questionRepository.findByUserId(userId);
     }
 
     // 리팩토링한 질문 검증 메소드
-    private void validateQuestion(final QuestionEntity questionEntity){
+    private void validateQuestion(final QuestionEntity questionEntity,boolean isAnnoymous){
         if(questionEntity==null){
             log.warn("Question Entity 는 null 이면 안됩니다.");
             throw new RuntimeException("Question Entity 는 null 이면 안됩니다.");
         }
-        if(questionEntity.getNickName()==null){
+        //isAnonymous 값이 true 일 때 닉네임 "익명"
+        if(isAnnoymous){
+            questionEntity.setNickName("익명");
+        }else if(questionEntity.getNickName()==null){
             log.warn("등록되지 않은 유저입니다.");
             throw new RuntimeException("등록되지 않은 유저입니다.");
+        }
+
+        if (questionEntity.getQuestionText()== null || questionEntity.getQuestionText().isEmpty()) {
+            throw new IllegalArgumentException("질문 내용을 입력하세요.");
         }
     }
 
@@ -65,14 +73,55 @@ public class QAService {
             log.warn("등록되지 않은 유저입니다.");
             throw new RuntimeException("등록되지 않은 유저입니다.");
         }
+
+        if (answerEntity.getAnswerText()== null || answerEntity.getAnswerText().isEmpty()) {
+            throw new IllegalArgumentException("답변 내용을 입력하세요.");
+        }
     }
 
     // 질문 등록 가능 - 익명/닉네임 질문 둘 다 가능하도록
-    public List<QuestionEntity> saveQuestion(final QuestionEntity questionEntity){
-        validateQuestion(questionEntity);
+    public List<QuestionEntity> saveQuestion(final QuestionEntity questionEntity,Boolean isAnonymous){
+        validateQuestion(questionEntity,isAnonymous);
         questionRepository.save(questionEntity);
         log.info("엔터티 아이디 : {} 가 저장되었습니다.", questionEntity.getId());
         return questionRepository.findByUserId(questionEntity.getNickName());
+    }
+
+    // 답변 등록 기능 - 당연히 닉네임으로
+    public List<AnswerEntity> saveAnswer(final AnswerEntity answerEntity){
+        validateAnswer(answerEntity);
+        answerRepository.save(answerEntity);
+        log.info("엔터티 아이디 : {} 가 저장되었습니다.", answerEntity.getId());
+        return answerRepository.findByUserId(answerEntity.getNickname());
+    }
+
+    // 특정 질문과 그에 대한 모든 답변을 삭제하는 기능
+    public List<QuestionEntity> deleteQuestionAndAnswers(String questionId){
+
+        // questionId에 해당하는 질문과 그에 대한 모든 답변을 가져옴
+        Optional<QuestionEntity> optionalQuestion=questionRepository.findById(questionId);
+        if(optionalQuestion.isPresent()){
+            QuestionEntity question=optionalQuestion.get();
+            List<AnswerEntity> answers=question.getAnswers();
+
+            try{
+                // 가져온 질문과 답변들을 삭제
+                answerRepository.deleteAll(answers);
+                questionRepository.delete(question);
+            }catch (Exception e){
+                log.error("질문 엔터티 삭제 중 에러 발생", questionId, e );
+                // 컨트롤러로 exception 을 보낸다. 데이터베이스 내부 로직을 캡슐화하려면 e를 리턴하지 않고 새 exception 오브젝트를 리턴한다
+                throw new RuntimeException("질문 엔터티 삭제 중 에러 발생" , e);
+                // 컨트롤러로 exception 을 보낸다. 데이터베이스 내부 로직을 캡슐화하려면 e를 리턴하지 않고 새 exception 오브젝트를 리턴한다
+//                throw new RuntimeException("답변 엔터티 삭제 중 에러 발생" + answers.get());
+            }
+
+        } else {
+            throw new IllegalArgumentException("질문을 찾을 수 없습니다.");
+        }
+
+        // 새로고침한 질문과 답변을 가져와 리턴한다
+        return getMyQuestions(questionId);
     }
 
 }
