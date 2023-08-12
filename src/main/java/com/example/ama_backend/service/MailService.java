1a63ec3f182611feb935586670cd0da99e649b03
suucong <sophia021210@naver.com>
2023-08-12 12:28:35 +0900

    fix: 메일관련 시크릿 유출 수정.

--- src/main/java/com/example/ama_backend/service/MailService.java
+++ src/main/java/com/example/ama_backend/service/MailService.java
@@ -3,10 +3,13 @@ package com.example.ama_backend.service;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 @Service
